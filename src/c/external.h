80d74d77a4343a37d577843ee960f6b6d6bb663e
Nathan Soufflet <nathsoufflet@gmail.com>
2025-04-25 19:17:09 +0200

    Remove duplicate C files

--- src/c/external.h
+++
@@ -1,9 +0,0 @@
-#ifndef SMB_EXTERNAL_H
-#define SMB_EXTERNAL_H
-
-#include <stddef.h>
-
-extern void* memset(void* ptr, int value, size_t num);
-extern void* memcpy(void* dst, const void* src, size_t num);
-
-#endif
